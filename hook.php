4fac23087babf703c3bbcdd0d77836b00e8c5c33
dravecca00 <ravecca@gmail.com>
2021-03-17 09:53:09 -0300

    Update hook.php

--- hook.php
+++ hook.php
@@ -38,9 +38,9 @@ $commands_paths = [
 
 $mysql_credentials = [
      'host'     => 'localhost',
-    'user'     => 'diego',
+    'user'     => '',
      'password' => '',
-     'database' => 'schedy',
+     'database' => '',
  ];
 
 try {
@@ -56,26 +56,7 @@ try {
     // Enable MySQL
     $telegram->enableMySql($mysql_credentials);
 
-    // Logging (Error, Debug and Raw Updates)
-    //Longman\TelegramBot\TelegramLog::initErrorLog(__DIR__ . "/{$bot_username}_error.log");
-    //Longman\TelegramBot\TelegramLog::initDebugLog(__DIR__ . "/{$bot_username}_debug.log");
-    //Longman\TelegramBot\TelegramLog::initUpdateLog(__DIR__ . "/{$bot_username}_update.log");
-
-    // If you are using a custom Monolog instance for logging, use this instead of the above
-    //Longman\TelegramBot\TelegramLog::initialize($your_external_monolog_instance);
-/*
-	TelegramLog::initialize(
-		// Main logger that handles all 'error' and 'debug' logs.
-		new Logger('telegram_bot', [
-			(new StreamHandler(__DIR__ . "/logs/{$bot_username}_debug.log", Logger::DEBUG))->setFormatter(new LineFormatter(null, null, true)),
-			(new StreamHandler(__DIR__ . "/logs/{$bot_username}_error.log", Logger::ERROR))->setFormatter(new LineFormatter(null, null, true)),
-		]),
-		// Updates logger for raw updates.
-		new Logger('telegram_bot_updates', [
-			(new StreamHandler(__DIR__ . "/logs/{$bot_username}_update.log", Logger::INFO))->setFormatter(new LineFormatter('%message%' . PHP_EOL)),
-		])
-	);
-*/
+   
 	TelegramLog::initialize(
 		// Main logger that handles all 'error' and 'debug' logs.
 		new Logger('telegram_bot', [
@@ -89,31 +70,10 @@ try {
      $telegram->setDownloadPath(__DIR__ . '/Download');
      $telegram->setUploadPath(__DIR__ . '/Upload');
 
-    // Here you can set some command specific parameters
-    // e.g. Google geocode/timezone api key for /date command
-    //$telegram->setCommandConfig('date', ['google_api_key' => 'your_google_api_key_here']);
-/*
-	$telegram->setCommandConfig('cleanup', [
-	      // Define which tables should be cleaned.
-	      'tables_to_clean' => [
-	          'message',
-	          'edited_message',
-	      ],
-	      // Define how old cleaned entries should be.
-	      'clean_older_than' => [
-	          'message'        => '3 days',
-	          'edited_message' => '3 days',
-	      ]
-		  ]
-	  );
-
-*/
-    // Botan.io integration
-    //$telegram->enableBotan('your_botan_token');
+  
 
     // Requests Limiter (tries to prevent reaching Telegram API limits)
     $telegram->enableLimiter();
-
 
 
 
